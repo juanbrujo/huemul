e250c7876428f1094f515aee02a64df9251d98f4
Jorge Epuñan <juanbrujo@gmail.com>
2022-04-18 13:47:43 -0400

    feat: new script product from neeks (#687)
    * feat(servel): replaced script to new elections
    
    * feat: new script neeks removed pethappy
    
    closes #685
    
    * test: passes
    
    * conflict solved

---
+++ scripts/neeks.js
@@ -0,0 +1,56 @@
+// Description:
+//   Get available products from Neeks
+//
+//  Dependencies:
+//    cheerio
+//
+// Commands:
+//   hubot neeks <product_name> - Get available products from Neeks
+//
+// Author:
+//   @hectorpalmatellez
+//   @jorgeepunan
+
+const cheerio = require('cheerio')
+
+module.exports = function (robot) {
+  robot.respond(/neeks (.*)/i, function (msg) {
+    const search = msg.match[1]
+    const mainUrl = 'https://neeks.cl/'
+    const url = mainUrl + '?s=' + search.replace(' ', '+') + '&post_type=product'
+
+    msg.send(':joystick: buscando ' + search + '...')
+
+    robot.http(url).get()(function (err, res, body) {
+      if (err || res.statusCode !== 200) {
+        robot.emit('error', err || new Error(`Status code is ${res.statusCode}`), msg, 'neeks')
+        msg.reply(':pinceleart: mató al animal')
+        return
+      }
+      const $ = cheerio.load(body)
+      const results = []
+
+      $('.badge.out-of-stock ').parents('.product-type-simple').addClass('no')
+
+      $('.product-type-simple:not(.no)').each(function () {
+        const $productName = $(this).find('.woocommerce-loop-product__title a').text()
+        const $productUrl = $(this).find('.woocommerce-loop-product__title a').attr('href')
+        const $productPrice = $(this).find('.woocommerce-Price-amount.amount bdi').text().trim()
+        results.push(' - ' + $productName + ': ' + '_' + $productPrice + '_\n  ' + $productUrl)
+      })
+
+      if (results.length > 0) {
+        const text = results.join('\n')
+
+        if (robot.adapter.constructor.name === 'SlackBot') {
+          const options = { unfurl_links: false, as_user: true }
+          robot.adapter.client.web.chat.postMessage(msg.message.room, text, options)
+        } else {
+          msg.send(text)
+        }
+      } else {
+        msg.send('No hay de lo que buscas, alégale a :pinceleart:')
+      }
+    })
+  })
+}
---
+++ test/neeks.test.js
@@ -0,0 +1,52 @@
+import 'coffee-script/register'
+import test from 'ava'
+import Helper from 'hubot-test-helper'
+import path from 'path'
+import nock from 'nock'
+
+const helper = new Helper('../scripts/neeks.js')
+const sleep = m => new Promise(resolve => setTimeout(() => resolve(), m))
+
+test.beforeEach(t => {
+  t.context.room = helper.createRoom({ httpd: false })
+})
+
+test.afterEach(t => t.context.room.destroy())
+
+test('Neeks Game Boy 200', async t => {
+  nock('https://neeks.cl/')
+    .get('?s=game+boy&post_type=product')
+    .replyWithFile(200, path.join(__dirname, 'html', 'neeks-200.html'))
+  t.context.room.user.say('user', 'hubot neeks game boy')
+  await sleep(500)
+
+  const user = t.context.room.messages[0]
+  const hubotMessage1 = t.context.room.messages[1]
+  const hubotMessage2 = t.context.room.messages[2]
+
+  // test user's message
+  t.deepEqual(user, ['user', 'hubot neeks game boy'])
+
+  // test hubot's response messages
+  t.deepEqual(hubotMessage1, ['hubot', ':joystick: buscando game boy...'])
+  t.deepEqual(hubotMessage2, ['hubot', '@user :pinceleart: mató al animal'])
+})
+
+test('Neeks 301', async (t) => {
+  nock('https://neeks.cl/')
+    .get('?s=301&post_type=product')
+    .reply(301)
+  t.context.room.user.say('user', 'hubot neeks 301')
+  await sleep(500)
+
+  const user = t.context.room.messages[0]
+  const hubotMessage1 = t.context.room.messages[1]
+  const hubotMessage2 = t.context.room.messages[2]
+
+  // test user's message
+  t.deepEqual(user, ['user', 'hubot neeks 301'])
+
+  // test hubot's response messages
+  t.deepEqual(hubotMessage1, ['hubot', ':joystick: buscando 301...'])
+  t.deepEqual(hubotMessage2, ['hubot', '@user :pinceleart: mató al animal'])
+})
